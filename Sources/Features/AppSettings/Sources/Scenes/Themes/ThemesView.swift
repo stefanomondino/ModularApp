673421ae7dc5fc1316e2dd724becf7a5e126f9be
Stefano Mondino <stefano.mondino.dev@gmail.com>
2025-06-30 22:40:27 +0200

    fix: restore preview for themes list

--- Sources/Features/AppSettings/Sources/Scenes/Themes/ThemesView.swift
+++ Sources/Features/AppSettings/Sources/Scenes/Themes/ThemesView.swift
@@ -52,15 +52,15 @@ extension Themes {
     }
 }
 
-// #Preview(traits: .design()) {
-//    Themes.ViewContents(viewModel: ThemesViewModelMock {
-//        $0.queryString = ""
-//        $0.items = (0 ..< 10).map { index in
-//            ThemeItemViewModelMock {
-//                $0.id = "theme-\(index)"
-//                $0.title = "Theme \(index + 1)"
-//                $0.colors = ["#FF5733", "#33FF57", "#3357FF", "#FF3357", "#5733FF"].shuffled()
-//            }
-//        }
-//    })
-// }
+#Preview(traits: .design()) {
+    Themes.ViewContents(viewModel: ThemesSceneViewModelMock {
+        $0.queryString = ""
+        $0.items = (0 ..< 10).map { index in
+            ThemeItemViewModelMock {
+                $0.id = "theme-\(index)"
+                $0.title = "Theme \(index + 1)"
+                $0.colors = ["#FF5733", "#33FF57", "#3357FF", "#FF3357", "#5733FF"].shuffled()
+            }
+        }
+    })
+}
